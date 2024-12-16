8c3395569fd33967164312733bd294c60b7a8c74
Kiëd Llaentenn <kiedtl@tilde.team>
2024-12-15 20:33:24 -0500

    feat: graphs etc

--- main.c
+++ main.c
@@ -13,8 +13,25 @@
 #include "sensirion_common.h"
 #include "sensirion_i2c_hal.h"
 
-uint8_t send_buf[128];
-size_t send_buf_len = 0;
+#define MY_LED 15
+
+#define HIST_LEN 127
+#define HIST_ITER(stuff) \
+	for (size_t i = 0, ind = co2_samples_ind - 1; i < HIST_LEN; ++i) { \
+		{ stuff; }; \
+		if (ind == 0) ind = HIST_LEN; \
+		ind -= 1; \
+	}
+
+#define WARN2(estr, ...) do { \
+	u8g2_DrawStr(&u8g2, 2, 8, estr); \
+	fprintf(stderr, __VA_ARGS__); \
+} while (false);
+
+#define ERROR2(estr, ...) do { \
+	u8g2_DrawStr(&u8g2, 2, 8, estr); \
+	ERROR(__VA_ARGS__); \
+} while (false);
 
 #define ERROR(...) do { \
 	fprintf(stderr, __VA_ARGS__); \
@@ -35,6 +52,9 @@ uhoh()
 uint8_t
 u8g2_byte_rpi_hw_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
 {
+	static uint8_t send_buf[128];
+	static size_t send_buf_len = 0;
+
 	switch (msg) {
 	break; case U8X8_MSG_BYTE_INIT:
 		i2c_init(i2c0, 100 * 1000);
@@ -98,9 +118,21 @@ u8g2_gpio_and_delay_rpi(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_pt
 	return 1;
 }
 
+uint8_t
+record(uint16_t *samples, uint8_t samples_ind, uint16_t sample)
+{
+	samples[samples_ind++] = sample;
+	if (samples_ind == HIST_LEN)
+		samples_ind = 0;
+	return samples_ind;
+}
+
 int
 main()
 {
+	gpio_init(MY_LED);
+	gpio_set_dir(MY_LED, GPIO_OUT);
+
 	stdio_init_all();
 	if (cyw43_arch_init()) {
 		ERROR("Cyw43 init failed.");
@@ -124,36 +156,45 @@ main()
 
 	int16_t scd_error = 0;
 	uint16_t co2;
+	uint16_t co2_samples[HIST_LEN] = {0};
+	uint8_t co2_samples_ind = 0;
 	float temperature, humidity;
 
 	scd4x_wake_up();
 	scd4x_stop_periodic_measurement();
+	scd4x_set_automatic_self_calibration(false);
 	scd4x_reinit();
 
 	scd_error = scd4x_start_periodic_measurement();
 	if (scd_error) {
-		ERROR("Couldn't start measurement: %d\n", scd_error);
+		ERROR2("E_NOSTART", "Couldn't start measurement: %d\n", scd_error);
 	}
 
-	while ("I hate this") {
-		bool data_ready_flag = false;
-		sensirion_i2c_hal_sleep_usec(50000);
-		scd_error = scd4x_get_data_ready_flag(&data_ready_flag);
+	while ("static types are the best") {
+		gpio_put(MY_LED, 1);
+		sleep_ms(1200);
+		gpio_put(MY_LED, 0);
+		sleep_ms(1200);
+
+		bool is_data_ready = false;
+		scd_error = scd4x_get_data_ready_flag(&is_data_ready);
 		if (scd_error) {
-			//fprintf(stderr, "Error executing scd4x_get_data_ready_flag(): %i\n ", scd_error);
+			ERROR("E_NOREADY", "Error when getting data ready: %i\n ", scd_error);
 			continue;
 		}
 
-		if (!data_ready_flag) {
+		if (!is_data_ready) {
 			continue;
 		}
 
 		scd_error = scd4x_read_measurement(&co2, &temperature, &humidity);
 		if (scd_error) {
-			ERROR("Error executing scd4x_read_measurement(): %i\n", scd_error);
+			ERROR2("E_NOMEASURE", "Error measuring: %i\n", scd_error);
 		} else if (co2 == 0) {
-			ERROR("Invalid sample detected, skipping.\n");
+			WARN2("E_INVMEASURE", "Invalid sample detected, skipping.\n");
 		} else {
+			co2_samples_ind = record((uint16_t *)&co2_samples, co2_samples_ind, co2);
+
 			temperature *= 9;
 			temperature /= 5;
 			temperature += 32;
@@ -166,7 +207,38 @@ main()
 				"%4d C | %.01f\xb0""F | %.01f RH",
 				co2, temperature, humidity
 			);
-			u8g2_DrawStr(&u8g2, 1, 8, (char *)&buf);
+			u8g2_DrawStr(&u8g2, 2, 8, (char *)&buf);
+
+			size_t min = 999999;
+			size_t max = 666;
+
+			HIST_ITER(
+				if (co2_samples[ind] > max) max = co2_samples[ind];
+				if (co2_samples[ind] < min) min = co2_samples[ind];
+			);
+			max = max > 4000 ? 4000 : max;
+			min = min <  200 ?  200 : min;
+			min = min >  900 ?  900 : min;
+
+			HIST_ITER({
+				size_t x = 127 - i;
+				double s = co2_samples[ind];
+				s = s > min ? s - min : 0;
+				s = s > max ? max : s;
+				s = s / (max - min);
+				size_t y = (size_t)(s * (64 - 16));
+				y = (64 + 16) - y;
+				u8g2_DrawPixel(&u8g2, x, y);
+			});
+
+			memset(buf, 0, 32);
+			snprintf((char *)&buf, 32, "%4d ", max);
+			u8g2_DrawStr(&u8g2, 0, 16 + 8, (char *)&buf);
+
+			memset(buf, 0, 32);
+			snprintf((char *)&buf, 32, "%4d ", min);
+			u8g2_DrawStr(&u8g2, 0, 64, (char *)&buf);
+
 			u8g2_SendBuffer(&u8g2);
 		}
 	}
